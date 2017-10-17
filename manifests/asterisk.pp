b589c744e02d7755131aceafd284be27534b91f3
root <root@puppet.azimuth.holding.local>
2017-10-17 11:00:46 +0500

    added asterisk manager port to secure

--- manifests/asterisk.pp
+++ manifests/asterisk.pp
@@ -10,12 +10,19 @@ class fail2ban::asterisk {
 		notify => Service['fail2ban'],
 	}
 	ini_setting {'fail2ban_asterisk_enable':
-		require => Ini_setting['fail2ban_asterisk_bantime'],
 		path => '/etc/fail2ban/jail.conf',
 		ensure => present,
 		section => 'asterisk',
 		setting => 'enabled',
 		value => 'true',
+		notify => Service['fail2ban'],
+	}
+	ini_setting {'fail2ban_asterisk_ports':
+		path => '/etc/fail2ban/jail.conf',
+		ensure => present,
+		section => 'asterisk',
+		setting => 'port',
+		value => '5060,5061,5038,8088',
 		notify => Service['fail2ban'],
 	}
 }
